848ee7b11f0723b70b7f1bcd861f37e23abde7d9
zuhaib <zuhaibiqbalsani26@gmail.com>
2023-02-01 16:17:33 +0500

    JS Quiz added

--- Class 12 JS Quiz/index.js
+++ Class 12 JS Quiz/index.js
@@ -45,43 +45,4 @@ show("The number is: " +num+ " and the formatted num is: "+formatted);
 
 
 
-// SECTION III
-// Quetion 4
 
-// SECTION IV
-// MATRIX MULTIPLICATION
-
-// function matrixMultiplication(m1, m2) {
-//   let fil_m1 = m1.length;
-//   let col_m1 = m1[0].length;
-//   let fil_m2 = m2.length;
-//   let col_m2 = m2[0].length;
-
-//   if (col_m1 != fil_m2) return "Matrices cannot be multiplied";
-
-//   let multiplication = new Array(fil_m1);
-//   for (x = 0; x < multiplication.length; x++)
-//     multiplication[x] = new Array(col_m2).fill(0);
-
-//   for (x = 0; x < multiplication.length; x++) {
-//     for (y = 0; y < multiplication[x].length; y++) {
-//       for (z = 0; z < col_m1; z++) {
-//         multiplication[x][y] = multiplication[x][y] + m1[x][z] * m2[z][y];
-//       }
-//     }
-//   }
-
-//   return multiplication;
-// }
-
-// let m1 = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-// ];
-// let m2 = [
-//   [7, 8],
-//   [9, 10],
-//   [11, 12],
-// ];
-
-// show(matrixMultiplication(m1, m2));
